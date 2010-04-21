8b30da97f5a948bf5438ccb86873b039f6fa049f
jpflorijn@gmail.com <jpflorijn@gmail.com@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-04-21 10:12:44 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@30 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/LaserSwarm.java
+++ simulator/src/com/google/code/laserswarm/LaserSwarm.java
@@ -1,17 +1,10 @@
 package com.google.code.laserswarm;
 
-import java.io.File;
-
-import org.simpleframework.xml.Serializer;
-import org.simpleframework.xml.core.Persister;
-
 import com.google.code.laserswarm.conf.Configuration;
 
 public class LaserSwarm {
-	public static void main(String[] args) throws Exception {
-		Serializer serializer = new Persister();
+	public static void main(String[] args) {
 		Configuration example = new Configuration();
-		File result = new File("example.xml");
-		serializer.write(example, result);
+		example.write("configuration.xml");
 	}
 }
--- simulator/src/com/google/code/laserswarm/conf/Configuration.java
+++ simulator/src/com/google/code/laserswarm/conf/Configuration.java
@@ -1,13 +1,17 @@
 package com.google.code.laserswarm.conf;
 
+import java.io.File;
 import java.util.List;
 
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
 import com.google.common.collect.Lists;
+import com.lyndir.lhunath.lib.system.logging.Logger;
 
 @Root
 public class Configuration {
@@ -48,6 +52,8 @@ public class Configuration {
 		return constellations;
 	}
 	
+	private static final Logger	logger	= Logger.get(Configuration.class);
+	
 	public Configuration() {
 		constellations.add(new Constellation());
 		constellations.add(new Constellation());
@@ -60,5 +66,24 @@ public class Configuration {
 		fileNameReport = report;
 		fileNameScatterModel = model;
 		constellations = consts;
+	}
+	
+	public boolean write(String filename)
+	{
+		boolean success = true;
+		Serializer serializer = new Persister();
+		File result = new File(filename);
+		try {
+			serializer.write(this, result);
+		} catch (Exception e) {
+			success = false;
+			logger.inf(e, "Configuration file writing failed");
+		}
+		return success;
+	}
+	
+	public boolean read(String filename)
+	{
+		return true;
 	}
 }
