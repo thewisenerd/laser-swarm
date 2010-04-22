adb978d91bb335b8050638aa19676318e2b281c8
AODtorusan <AODtorusan@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-04-22 09:09:59 +0000

    Added ScatteringCharacteristics
    git-svn-id: svn://localhost/tmp/testgc/trunk@49 230b2fa6-915b-415b-b5c9-3ad8ff460498

---
+++ simulator/src/com/google/code/laserswarm/earthModel/ScatteringCharacteristics.java
@@ -0,0 +1,7 @@
+package com.google.code.laserswarm.earthModel;
+
+import com.google.code.laserswarm.math.Distribution;
+
+public abstract class ScatteringCharacteristics implements Distribution {
+
+}
