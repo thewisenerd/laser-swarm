e4bed6a7cfa00ec58f41340da440bfbf699df086
jpflorijn@gmail.com <jpflorijn@gmail.com@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-04-28 12:59:33 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@249 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/test/com/google/code/laserswarm/TestHeightDistributionPlot.java
+++ simulator/test/com/google/code/laserswarm/TestHeightDistributionPlot.java
@@ -1,20 +1,14 @@
 package com.google.code.laserswarm;
 
-import java.io.File;
-
-import org.geotools.geometry.DirectPosition2D;
-
-import com.google.code.laserswarm.earthModel.IElevationModel;
 import com.google.code.laserswarm.out.plot1D.plotHeightDistribution;
+import com.google.code.laserswarm.simulation.Simulator;
 import com.google.code.laserswarm.util.demReader.DemCreationException;
-import com.google.code.laserswarm.util.demReader.DemReader;
 
 public class TestHeightDistributionPlot {
 	public static void main(String[] args) throws DemCreationException {
 		plotHeightDistribution plot = new plotHeightDistribution();
-		IElevationModel DEM = DemReader.parseDem(new File("DEM/srtm_37_02-red.asc"));
-		DirectPosition2D st = new DirectPosition2D(3.55, 50.5);
-		DirectPosition2D sp = new DirectPosition2D(3.6, 51);
-		plot.plot(st, sp, DEM, 100, 3, "../report/simulator/img/testHeightPlot");
+		for (Simulator sim : SimulationTester.sim().values()) {
+			plot.plot(sim.getDataPoints(), 3, "../report/simulator/img/testHeightPlot");
+		}
 	}
 }
