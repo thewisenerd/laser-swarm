7ea0d11814e8ef0e1ed2b198263adedb5c0cb1fe
AODtorusan <AODtorusan@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-05-04 07:23:33 +0000

    Typo
    git-svn-id: svn://localhost/tmp/testgc/trunk@414 230b2fa6-915b-415b-b5c9-3ad8ff460498

---
+++ simulator/src/com/google/code/laserswarm/process/EmitterHistory.java
@@ -0,0 +1,33 @@
+package com.google.code.laserswarm.process;
+
+import java.util.List;
+import java.util.TreeSet;
+
+import com.google.code.laserswarm.conf.Constellation;
+import com.google.code.laserswarm.simulation.SimVars;
+import com.google.common.collect.Sets;
+
+public class EmitterHistory {
+
+	private Constellation	constellation;
+	private double			halfPulseTime;
+	private TreeSet<Double>	time	= Sets.newTreeSet();
+
+	public EmitterHistory(Constellation constellation, List<SimVars> dataSet) {
+		this.constellation = constellation;
+		halfPulseTime = (1 / constellation.getPulseFrequency()) / 2;
+
+		for (SimVars simVars : dataSet)
+			time.add(simVars.t0);
+	}
+
+	public double getPulseBeforePulse(double t) {
+		t = t - halfPulseTime;
+		return time.floor(t);
+	}
+
+	public double getPulseClosesTo(double t) {
+		return time.floor(t);
+	}
+
+}
