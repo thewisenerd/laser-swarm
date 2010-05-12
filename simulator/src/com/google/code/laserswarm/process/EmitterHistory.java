e04ef8afc006bb99c4a882cff3926c0ec877ed18
AODtorusan <AODtorusan@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-05-12 14:19:01 +0000

    Added emmitor lookuptable
    git-svn-id: svn://localhost/tmp/testgc/trunk@571 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/process/EmitterHistory.java
+++ simulator/src/com/google/code/laserswarm/process/EmitterHistory.java
@@ -2,9 +2,10 @@ package com.google.code.laserswarm.process;
 
 import java.util.List;
 import java.util.TreeSet;
- 
+
 import com.google.code.laserswarm.conf.Constellation;
 import com.google.code.laserswarm.conf.Satellite;
+import com.google.code.laserswarm.math.LookupTable;
 import com.google.code.laserswarm.simulation.SimVars;
 import com.google.common.collect.Sets;
 
@@ -13,14 +14,26 @@ public class EmitterHistory {
 	private Constellation	constellation;
 	private double			halfPulseTime;
 	public TreeSet<Double>	time	= Sets.newTreeSet();
+	private LookupTable		position;
 
 	public EmitterHistory(Constellation constellation, List<SimVars> dataSet) {
 		this.constellation = constellation;
 		halfPulseTime = (1 / constellation.getPulseFrequency()) / 2;
+		position = new LookupTable();
 
-		for (SimVars simVars : dataSet)
+		for (SimVars simVars : dataSet) {
+			position.put(simVars.t0, simVars.p0);
 			time.add(simVars.t0);
-	
+		}
+
+	}
+
+	public Satellite getEm() {
+		return constellation.getEmitter();
+	}
+
+	public LookupTable getPosition() {
+		return position;
 	}
 
 	public double getPulseBeforePulse(double t) {
@@ -30,9 +43,5 @@ public class EmitterHistory {
 
 	public double getPulseClosesTo(double t) {
 		return time.floor(t);
-	}
-     
-	public Satellite getEm(){
-		return constellation.getEmitter(); 
 	}
 }
