f3e3da62bc76c896389797375560c98f55d04f0c
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-05-28 14:32:29 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@885 230b2fa6-915b-415b-b5c9-3ad8ff460498

---
+++ simulator/src/com/google/code/laserswarm/Desim/FindBRDF.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.google.code.laserswarm.Desim;
+
+/**
+ * @author Administrator
+ *
+ */
+public class FindBRDF {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 
+		throw new UnsupportedOperationException();
+	}
+
+}
