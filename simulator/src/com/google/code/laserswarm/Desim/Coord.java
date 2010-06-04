325193c3a7b1d8cf3c48064bbd438a7e3ef3156e
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-04 14:55:13 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@981 230b2fa6-915b-415b-b5c9-3ad8ff460498

---
+++ simulator/src/com/google/code/laserswarm/Desim/Coord.java
@@ -0,0 +1,55 @@
+package com.google.code.laserswarm.Desim;
+
+import javax.vecmath.Vector2d;
+
+import org.apache.commons.math.geometry.Vector3D;
+/**
+ * Coord class stores the pair of vectors
+ * vecI - input vector
+ * vecR - reflecting vector
+ * @author Administrator
+ *
+ */
+public class Coord {
+	/**
+	 * x coord = [0..pi/2]	elevation
+	 * y coord = [-pi..pi] rotation
+	 */
+	public Vector2d vecI;		//angle incident wrt normal 
+	/**
+	 * Reflection vector 
+	 * 
+	 */
+	public Vector2d vecR;		//angle reflected wrt normal 
+	
+	public Coord() {			//Vector
+	vecI = new Vector2d();			//x azimuth in degrees
+	vecR = new Vector2d();			//y offset in degrees 
+	
+	}
+	/**
+	 * Assuming that the VecI is from zenith [0,0]
+	 * @param thR vector of reflection ;
+	 */
+	public Coord(Vector2d thR){ //zenith coordinate  
+		this.vecR = new Vector2d(thR);
+		this.vecI = new Vector2d(new double[]{0,0});
+	}
+
+	public Coord(Vector2d thR,Vector2d thI){ //zenith coordinate  
+		this.vecR = new Vector2d(thR);
+		this.vecI = new Vector2d(thI);
+	}
+	/**
+	 * 
+	 * @param val
+	 */
+	private void incI(double val) {
+		
+	}
+	private void incR() {
+		// TODO Auto-generated method stub
+		// 
+		throw new UnsupportedOperationException();
+	}
+}
