6c1e853c9d5c7c52f242cba374b6a7dbc6fd73cc
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-07 15:30:25 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@1019 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/Desim/Sphere.java
+++ simulator/src/com/google/code/laserswarm/Desim/Sphere.java
@@ -4,15 +4,18 @@
 package com.google.code.laserswarm.Desim;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import javax.vecmath.*;
 
 /**
  * @author Administrator
  *
  */
-public class Sphere  extends TreeMap<Coord, Double>{
+public class Sphere  extends TreeMap<Vector3d, Double>{
 	
+	double total;
 /*	class Comp implements Comparator<Coord>{
 
 		@Override
@@ -29,13 +32,33 @@ public class Sphere  extends TreeMap<Coord, Double>{
 	/**
 	 * Rotate the points around the center  in the horizontal plane 
 	 */
-	public void rotate(double rad){		//rotate around x-axis
-		for (Map.Entry<Coord, Double> tmpIt : entrySet()){
+	@Override
+	public Double put(Vector3d key, Double value) {
+		// TODO Auto-generated method stub
+		total++;
+		if(!this.containsKey(key)){
+			return super.put(key, value);
+		}
+		else{
+			return super.put(key, get(key)+value); //add value
+		}
+
+		
+	}
+/*	public void rotate(double rad){		//rotate around x-axis
+		
+		for (Map.Entry<Vector2d, ResVec> tmpIt : entrySet()){
 			tmpIt.getKey().vecI.y += rad;
 			tmpIt.getKey().vecR.y += rad;
 		}
 		
-		}
+		}*/
+	@Override
+	public Double get(Object key) {
+		 Double tmp = super.get(key);
+		 return tmp/total;
+		
+	}
 	public Sphere() {
 		// TODO Auto-generated constructor stub
 	super();
