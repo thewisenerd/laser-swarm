f85c4a5912ee5146bf82d2603febd3c4c2374328
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-08 15:54:49 +0000

    Class that defines region for which the BRDF is computed
    git-svn-id: svn://localhost/tmp/testgc/trunk@1047 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/Desim/BRDFcalc/Region.java
+++ simulator/src/com/google/code/laserswarm/Desim/BRDFcalc/Region.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 import javax.vecmath.Vector3d;
 
+import com.google.common.collect.Lists;
+
 /**
  * @author Administrator
  *
@@ -15,14 +17,33 @@ import javax.vecmath.Vector3d;
 public class Region {
 	BRDF brdf;
 	Boundries bound; 
+	boolean hasAll;
+	boolean hasNone;
 	LinkedList<Vector3d> path;		//List of points in ECEF
 	
+	public Region(Boundries bnd) {
+		this.brdf = new BRDF(false); // create non symetric brdf 
+		this.path = Lists.newLinkedList();
+		this.hasAll = false;
+		this.hasNone = true;
+		this.bound = bnd;	
+		
+	}
+	public Region(boolean empty) {
+		this.brdf = new BRDF(false); // create non symetric brdf 
+		this.path = Lists.newLinkedList();
+		
+		hasAll = false;
+		hasNone = true;
+	}
 	public void add(Map<Vector3d,Double> recMap, Vector3d loc) {
 	brdf.add(recMap);
 	path.add(loc);
 	}
 	
 	public	boolean contains(Vector3d vec) {
+		if(hasAll) return true;
+		if(hasNone) return false;
 		return bound.contains(vec);
 	}
 	
