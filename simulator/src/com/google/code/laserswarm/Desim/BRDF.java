a1aad4e46c86a070bff2c343d6962e421b37c545
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-08 12:00:10 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@1031 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/Desim/BRDF.java
+++ simulator/src/com/google/code/laserswarm/Desim/BRDF.java
@@ -9,17 +9,20 @@ import javax.vecmath.Vector2d;
 import javax.vecmath.Vector3d;
 
 import com.google.code.laserswarm.math.Distribution;
+import com.lyndir.lhunath.lib.system.logging.Logger;
 
 /**
  * @author Administrator
  *
  */
 public class BRDF extends Distribution  {	
+	
 		
 	Sphere brdf;			//sphere that represents the brdf 
 	Vector3d direction; 	//direction of the origin
 	Vector3d inDir;		//direction of incidence
 	boolean symetric;		//the brdf is symetric around the zenith 
+	private static final Logger	logger		= Logger.get(BRDF.class);
 	
 	
 	public BRDF(boolean sym) {
@@ -32,25 +35,37 @@ public class BRDF extends Distribution  {
 			
 		}
 	}
+	/**
+	 * add point to the brdf
+	 * @param direction vector
+	 * @param number of photons
+	 */
 	public void add(Vector3d dir, double num ) {
 		brdf.put(dir, num);
 		
 	}
+	/**
+	 * add 1 photon in the given direction
+	 * @param direction vector
+	 */
 	public void add(Vector3d dir) {
 		brdf.put(dir, 1.0);
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 
 		throw new UnsupportedOperationException();
 	}
+	
 	@Override
 	public double probability(Vector3d x) {
 		Double tmp;
 		try{
-		 tmp =brdf.get(x);
+		 tmp = brdf.get(x);
 	}catch(NullPointerException e){
-		 tmp = (double) 0;
+		logger.dbg(e,"");
+		tmp = (double) 0;
 	}
 	
 		return	 tmp;
