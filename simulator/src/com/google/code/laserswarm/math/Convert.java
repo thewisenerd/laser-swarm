411fc64c13ec6ab395241c115e9b659248ed3b1d
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-08 15:23:24 +0000

    added toENU
    git-svn-id: svn://localhost/tmp/testgc/trunk@1041 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/math/Convert.java
+++ simulator/src/com/google/code/laserswarm/math/Convert.java
@@ -1,5 +1,7 @@
 package com.google.code.laserswarm.math;
 
+import java.util.Vector;
+
 import javax.vecmath.Point3d;
 import javax.vecmath.Tuple3d;
 import javax.vecmath.Vector3d;
@@ -35,6 +37,37 @@ public abstract class Convert {
 		double y = r * Math.cos(phi) * Math.sin(theta);
 		double z = r * Math.sin(phi);
 		return new Point3d(x, y, z);
+	}
+	/**
+	 * Convert from ECEF to ENU
+	 * @param coord in ECEF
+	 * @return coordinates in ENU
+	 */
+	
+	public static Vector3d toENU(Tuple3d coord) {
+		double a = coord.x;
+		double b =  coord.y;
+		double c=  coord.z;
+		 Vector3d vec = new Vector3d(coord);
+		 Vector3d proj = new Vector3d(a, b, 0.0);	//projection ( no z axis) 
+		 
+		 double lat = vec.angle(proj);
+		 double lon = proj.angle(new Vector3d(1,0,0));		//xaxis
+		 
+		 double slon = Math.sin(lon);
+		 double slat = Math.sin(lat);
+		 double clon = Math.cos(lon);
+		 double clat = Math.cos(lat);
+		 
+		 
+		 
+		 double x = -1*slon*a + clon*b;
+		 double y = -1*slat*clon*a  -slat*slon*b + clat*c;
+		 double z = clat*clon*a + clat*slon*b  + slat*c;
+		 
+		 
+	
+		return new Vector3d(x,y,z);
 	}
 
 }
