4370d4b70606588710b82ae56172a69bf902d78e
vedrosopleij <vedrosopleij@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-08 12:00:27 +0000

    git-svn-id: svn://localhost/tmp/testgc/trunk@1032 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/Desim/FindBRDF.java
+++ simulator/src/com/google/code/laserswarm/Desim/FindBRDF.java
@@ -3,12 +3,46 @@
  */
 package com.google.code.laserswarm.Desim;
 
+import java.util.LinkedList;
+import java.util.Map;
+
+import javax.vecmath.Vector3d;
+
+import com.google.code.laserswarm.Orbit.OrbConv;
+
 /**
+ * BRDFs point north by default
+ * 
  * @author Administrator
  *
  */
-public class FindBRDF {
 
+public class FindBRDF {
+	BRDF brdf;
+	Region region;
+	OrbConv conv;
+	/**
+	 * from reflector coordinates 
+	 * @param emVec
+	 * @param recVecs
+	 * @param emDir
+	 * @param reg
+	 */
+	private void putDataToRegion(Vector3d emVec, Map<Vector3d,Double> recVecs, Vector3d emDir,Region reg ){
+	reg.add(recVecs, emVec);
+		
+	}
+	/**
+	 * Input ECI output reflector coordinates
+	 * @param emVec
+	 */
+	private void convRefCoord(Vector3d emVec,Map<Vector3d,Double> recVecs, double alt) {
+		
+		
+	}
+	
+	
+	
 	/**
 	 * @param args
 	 */
