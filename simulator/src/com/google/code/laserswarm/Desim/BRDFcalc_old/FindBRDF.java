91394ca2b6a9b51c5f1aba11f8d0a8e5319f3f73
AODtorusan <AODtorusan@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-16 12:58:34 +0000

    Moved old files
    git-svn-id: svn://localhost/tmp/testgc/trunk@1317 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/Desim/BRDFcalc_old/FindBRDF.java
+++ simulator/src/com/google/code/laserswarm/Desim/BRDFcalc_old/FindBRDF.java
@@ -3,16 +3,12 @@
  */
 package com.google.code.laserswarm.Desim.BRDFcalc_old;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.vecmath.Vector3d;
 
+import com.google.code.laserswarm.Desim.BRDF;
 import com.google.code.laserswarm.Orbit.OrbConv;
-import com.google.code.laserswarm.math.Convert;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.lyndir.lhunath.lib.system.logging.Logger;
 
 /**
  * BRDFs point north by default
@@ -23,72 +19,36 @@ import com.lyndir.lhunath.lib.system.logging.Logger;
 
 public class FindBRDF {
 
-	private Region				defRegion;
-	private List<Region>		region;
-	private OrbConv				conv;
-
-	private static final Logger	logger	= Logger.get(FindBRDF.class);
-
-	public FindBRDF() {
-		conv = new OrbConv();
-		defRegion = new Region(false);
-		region = Lists.newArrayList();
-		// region.add();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 
+		throw new UnsupportedOperationException();
 	}
 
+	BRDF	brdf;
+	Region	region;
+	OrbConv	conv;
+
 	/**
+	 * Input ECI output reflector coordinates
 	 * 
 	 * @param emVec
-	 *            ECI vector for emitter with altitude subtracted (points to the base of the plane )
-	 * @param recVecs
-	 *            ECI vector for receivers
 	 */
-	public void add(Vector3d emVecECI, Map<Vector3d, Double> recVecsECI, double t_cur) {
-		// convert emVec to ECEF
-		// convert recVecs to ECEF to ENU see
-		// http://en.wikipedia.org/wiki/Geodetic_system#From_ECEF_to_ENU
-		Map<Vector3d, Double> recVecsECU = Maps.newHashMap();
-		Vector3d emVecECEF = conv.ECEF_vec(t_cur, emVecECI); // convert to ECEF
+	private void convRefCoord(Vector3d emVec, Map<Vector3d, Double> recVecs, double alt) {
 
-		for (Vector3d recIt : recVecsECI.keySet()) {
-			Vector3d hm = Convert.toENU(conv.ECEF_vec(t_cur, recIt));
-			recVecsECU.put(hm, recVecsECI.get(recIt)); // CONVERT to ECU
-
-		}
-
-		putDataToRegion(emVecECEF, recVecsECU);
 	}
 
 	/**
 	 * from reflector coordinates
 	 * 
 	 * @param emVec
-	 *            emitter pos in ECEF
 	 * @param recVecs
-	 *            vectors ENU
 	 * @param emDir
-	 *            Not needed
 	 * @param reg
 	 */
-	private void putDataToRegion(Vector3d emVec, Map<Vector3d, Double> recVecs) {
-		Region reg = selectRegion(emVec); // select region
+	private void putDataToRegion(Vector3d emVec, Map<Vector3d, Double> recVecs, Vector3d emDir,
+			Region reg) {
 		reg.add(recVecs, emVec);
-	}
-
-	/**
-	 * Selects the required region from the list to store the BRDF
-	 * 
-	 * @param emVec
-	 *            emitter vector
-	 * @return
-	 */
-	private Region selectRegion(Vector3d emVec) {
-		for (Region regIt : region) {
-			if (regIt.contains(emVec))
-				return regIt;
-		}
-		logger.dbg("returning default regoin");
-		return defRegion;
 
 	}
 
