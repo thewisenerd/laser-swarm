8eb210d10d9eeb92079b6f8ffec4d5494ddf4fb8
AODtorusan <AODtorusan@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-18 15:35:33 +0000

    New gui tools (split gui for 2 images)
    git-svn-id: svn://localhost/tmp/testgc/trunk@1507 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/util/GuiFactory.java
+++ simulator/src/com/google/code/laserswarm/util/GuiFactory.java
@@ -1,8 +1,42 @@
 package com.google.code.laserswarm.util;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 public abstract class GuiFactory {
+
+	public static JFrame dualImageGui(final Image im1, final Image im2) {
+		JFrame fr = getDefaultJFrame("Image comparison");
+
+		JPanel left = new JPanel() {
+			@Override
+			public void paintComponent(java.awt.Graphics g) {
+				super.paintComponent(g);
+				Graphics2D g2 = Graphics2D.class.cast(g);
+				g2.drawImage(im1, 0, 0, getWidth(), getHeight(), this);
+			};
+		};
+		JPanel right = new JPanel() {
+			@Override
+			public void paintComponent(java.awt.Graphics g) {
+				super.paintComponent(g);
+				Graphics2D g2 = Graphics2D.class.cast(g);
+				g2.drawImage(im2, 0, 0, getWidth(), getHeight(), this);
+			};
+		};
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, left, right);
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation(fr.getWidth() / 2);
+
+		fr.add(splitPane);
+		fr.setVisible(true);
+		return fr;
+	}
 
 	public static JFrame getDefaultJFrame(String name) {
 		JFrame fr = new JFrame(name);
