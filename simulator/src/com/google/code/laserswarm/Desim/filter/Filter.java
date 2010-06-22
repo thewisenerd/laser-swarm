9e0fb2ac48454f45902e9a4050fe8cf735f10c8c
AODtorusan <AODtorusan@230b2fa6-915b-415b-b5c9-3ad8ff460498>
2010-06-22 12:11:32 +0000

    SVN duplicate name fix (see next commit)
    git-svn-id: svn://localhost/tmp/testgc/trunk@1621 230b2fa6-915b-415b-b5c9-3ad8ff460498

--- simulator/src/com/google/code/laserswarm/Desim/filter/Filter.java
+++
@@ -1,9 +0,0 @@
-package com.google.code.laserswarm.Desim.filter;
-
-import java.util.LinkedList;
-
-import javax.vecmath.Point3d;
-
-public interface Filter {
-	public LinkedList<Point3d> filter(LinkedList<Point3d> alts);
-}
--- simulator/src/com/google/code/laserswarm/Desim/filter/elevationslope/ElevationSlopeFilter.java
+++
@@ -1,8 +0,0 @@
-package com.google.code.laserswarm.Desim.filter.elevationslope;
-
-import com.google.code.laserswarm.Desim.elevation.slope.ElevationSlope;
-
-public interface ElevationSlopeFilter {
-	public ElevationSlope filter(ElevationSlope elSlopeData);
-
-}
